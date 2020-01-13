ff0d5e48a973c13467d81b7220744c885ffc78c5
terfno <sueda.takahito@gmail.com>
2020-01-13 16:21:42 +0900

    :zap::package: add gin

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/r-alvitr/hayabusa
 
 go 1.12
+
+require github.com/gin-gonic/gin v1.5.0
--- server.go
+++ server.go
@@ -1,9 +1,17 @@
 package main
 
 import (
-	"fmt"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Println("hello world")
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "yeah!",
+		})
+	})
+
+	r.Run(":8080")
 }
